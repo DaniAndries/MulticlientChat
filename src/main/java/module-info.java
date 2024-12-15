e9267fdbf8745c3efa8256c125f5eda7b819e397
DaniAndries <43108109+DaniAndries@users.noreply.github.com>
2024-12-15 15:25:59 +0100

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module thiar.dah.multiclientchat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens thiar.dah.multiclientchat to javafx.fxml;
+    exports thiar.dah.multiclientchat;
+}
